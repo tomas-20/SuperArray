01c560ce335c4163aa503af86631370a32901721
tomas-20 <tacuna20@stuy.edu>
2020-11-01 12:33:31 -0500

    added the basic functions

--- SuperArray.java
+++ SuperArray.java
@@ -5,4 +5,20 @@ public class SuperArray {
     data = new String[10];
     size = 0;
   }
+  public int size() {
+    return size;
+  }
+  public boolean add(String element) {
+    data[size] = element;
+    size ++;
+    return true;
+  }
+  public String get(int index) {
+    return data[index];
+  }
+  public String set(int index, String element) {
+    String output = data[index];
+    data[index] = element;
+    return output;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
 public class Tester {
   public static void main(String[] args) {
     SuperArray bob = new SuperArray();
+    System.out.println(bob.size());
+    System.out.println(bob.add("hello"));
+    System.out.println(bob.add("there"));
+    System.out.println(bob.size());
+    System.out.println(bob.set(0, "bye"));
+    System.out.println(bob.get(0));
   }
 }
