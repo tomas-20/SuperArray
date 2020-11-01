0b96e585caf8662f461b1b1bc740517c23ae02e1
tomas-20 <tacuna20@stuy.edu>
2020-11-01 14:17:58 -0500

    made a super fancy and consumer-friendly testing interface

--- Tester.java
+++ Tester.java
@@ -1,23 +1,14 @@
 public class Tester {
   public static void main(String[] args) {
     SuperArray bob = new SuperArray();
-    System.out.println(bob.size());
-    System.out.println(bob.add("hello"));
-    System.out.println(bob.add("there"));
-    System.out.println(bob.size());
-    System.out.println(bob.set(0, "bye"));
-    System.out.println(bob.get(0));
+    for (int i = 0; i < args.length; i ++) {
+      bob.add(args[i]);
+    }
     System.out.println(bob.toString());
-    bob.add("how");
-    bob.add("is");
-    bob.add("your");
-    bob.add("day");
-    bob.add("today");
-    bob.add("I");
-    bob.add("hope");
-    bob.add("it");
-    bob.add("is");
-    bob.add("good");
+    System.out.println(bob.size());
+    System.out.println(bob.get(2));
+    System.out.println("is the same as");
+    System.out.println(bob.set(2, "test"));
     System.out.println(bob.toString());
   }
 }
